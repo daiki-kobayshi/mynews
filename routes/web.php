1b2cff27c97af8c93a7f69019de6e63873131240
daiki kobayashi <daikichirikuto.gmal.com>
2020-07-03 09:15:22 +0000

    No19task4

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+  return view('welcome');
+});
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::group(['prefix'=>'admin'],function() {
-    Route::get('news/create','Admin/Newscontroller@add');
+    Route::get('news/create','Admin\NewsController@add');
     
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
