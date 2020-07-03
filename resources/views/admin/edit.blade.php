392c4ee1175c1610ac984563e4e61f9a4fc67ad1
daiki kobayashi <daikichirikuto.gmal.com>
2020-07-03 17:04:09 +0000

    No19task4

---
+++ resources/views/admin/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>basketball</title>
+    </head>
+    <body>
+        <h1>バスケットボール</h1>
+    </body>
+</html>
