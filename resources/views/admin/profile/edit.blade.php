6b1b780c8eebb44d8a847cebf32ba0c60dfc1202
daiki kobayashi <daikichirikuto.gmal.com>
2020-07-09 12:34:26 +0000

    No21task

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>basketball</title>
-    </head>
-    <body>
-        <h1>バスケットボール</h1>
-    </body>
-</html>
+@extends('layouts.admin')
+
+@section('title', '課題の新規作成')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <h2>No21task</h2>
+        </div>
+    </div>
+</div>
+@endsection
