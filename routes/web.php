24ccc7d934e54c2069991278523929949ba4f54d
daiki kobayashi <daikichirikuto.gmal.com>
2020-07-11 15:03:29 +0000

    No22

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix'=>'admin'],function() {
     Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create');
     
     Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
