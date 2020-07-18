94a10ca90586a7b13dc91360d503200dff6c496e
daiki kobayashi <daikichirikuto.gmal.com>
2020-07-18 00:16:23 +0000

    No26

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -22,7 +22,7 @@ class NewsController extends Controller
      $form = $request->all();
      
      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-     if (isset($form['image'])) {
+     if ($form['image']) {
          $path = $request->file('image')->store('public/image');
          $news->image_path = basename($path);
      } else {
@@ -39,4 +39,48 @@ class NewsController extends Controller
      
       return redirect('admin/news/create');
   }  
+  
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          $posts = News::where('title', $cond_title)->get();
+      } else {
+          $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+  
+  public function edit(Request $request)
+  {
+       // News Modelからデータを取得する
+      $news = News::find($request->id);
+      
+      return view('admin.news.edit', ['news_form' => $news]);
+  }
+  
+  public function update (Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, News::$rules);
+      // News Modelからデータを取得する
+      $news = News::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $news_form = $request->all();
+      unset($news_form['_token']);
+      
+      // 該当するデータを上書きして保存
+       $news->fill($news_form)->save();
+      
+      return redirect('admin/news');
+  }
+  
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $news = News::find($request->id);
+      // 削除する
+      $news->delete('admin/news/');
+  }
+  
 }
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,18 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix'=>'admin', 'middleware' => 'auth'],function() {
     Route::get('news/create','Admin\NewsController@add')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');;
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+     
     
     Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
     Route::post('profile/create','Admin\ProfileController@create');
     Route::post('profile/edit','Admin\ProfileController@update');
+    
+    
 });
 Auth::routes();
 
