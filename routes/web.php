22d0ffcbb862734dc0aeb8616038a28b8506c5f2
daiki kobayashi <daikichirikuto.gmal.com>
2020-07-26 18:49:09 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/', function () {
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix'=>'admin', 'middleware' => 'auth'],function() {
-    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');;
-    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/create','Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
     Route::get('news/edit', 'Admin\NewsController@edit');
     Route::post('news/edit', 'Admin\NewsController@update');
     Route::get('news/delete', 'Admin\NewsController@delete');
@@ -34,6 +34,5 @@ Route::group(['prefix'=>'admin', 'middleware' => 'auth'],function() {
 });
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'NewsController@index');
 Route::get('/profile', 'ProfileController@index');
